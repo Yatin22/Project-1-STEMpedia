75b9880bf544ac0bf6c92eeac51566b94ef005a5
Yatin jhamb <yatinjhamb10@gmail.com>
2023-06-15 13:10:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { createSlice } from '@reduxjs/toolkit';
+import './App.css'
+const valueSlice = createSlice({
+  name: 'value',
+  initialState: '',
+  reducers: {
+    updateValue: (state, action) => {
+      return action.payload;
+    },
+  },
+});
 
-function App() {
+const { updateValue } = valueSlice.actions;
+
+
+
+const ComponentB = () => {
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state);
+
+  const handleChange = (event) => {
+    dispatch(updateValue(event.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Component B</h2>
+      <input
+        type="text"
+        className="input-field"
+        value={value}
+        onChange={handleChange}
+        placeholder='Enter the value'
+      />
     </div>
   );
-}
+};
 
-export default App;
+const ComponentC = () => {
+  const value = useSelector((state) => state);
+
+  return (
+    <div>
+      <h2>Component C</h2>
+      <p>Value from Redux store: {value}</p>
+      {value.length > 0 && <p>Length of value: {value.length}</p>}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Component A</h1>
+      <div className="components-container">
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </div>
+  );
+};
+
+export {valueSlice , App};
